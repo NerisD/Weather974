32516c9b805c5685c9caf4cd18a7ebc3095ae95c
Dimitri SMITH <dsmith@videlio-lab.com>
2015-07-05 10:58:51 +0200

    initial Commit

---
+++ ListWeatherCity/ListWeatherCityViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ListWeatherCityViewController.swift
+//  Weather974
+//
+//  Created by Dimitri SMITH on 05/07/2015.
+//  Copyright (c) 2015 Dimiri SMITH. All rights reserved.
+//
+
+import UIKit
+
+class ListWeatherCityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
