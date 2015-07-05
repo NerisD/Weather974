b89a6e4471193e0dd780b9358ad5764184feda87
Dimitri SMITH <dsmith@videlio-lab.com>
2015-07-05 10:54:02 +0200

    Initial Commit

---
+++ Weather974/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather974
+//
+//  Created by Dimitri SMITH on 05/07/2015.
+//  Copyright (c) 2015 Dimiri SMITH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
